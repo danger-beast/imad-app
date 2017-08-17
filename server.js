283c3a6ddbbb8b0d85e0d26f4b0b8a3b3888e320
Muhamad Uzair <cool.uzair2000@gmail.com>
2017-08-17 13:58:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', htmlTemplate));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
