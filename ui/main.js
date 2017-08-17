d0132da7ce583679d6118d7c04ec4afdfb42005a
Muhamad Uzair <cool.uzair2000@gmail.com>
2017-08-17 14:44:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,7 @@
 console.log('Loaded!');
 
 var img = document.getElementById('madi');
-
-var i = 0;
-
+  
 img.onclick = function() {
-  if(i === 0 ) {
-   img.style.marginRight = '100px';
-   i++;
-  } else {
-   img.style.marginLeft = '100px';   
-   i = 0;
-  }
+  img.style.marginLeft = '100px';  
 };
