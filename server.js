8c538588a028c9aa749a0379d1e6f978e3dab94c
Muhamad Uzair <cool.uzair2000@gmail.com>
2017-08-17 13:58:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,19 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title: "Article One | Uzair",
     heading: "Article First Heading",
     date: "Sept 5, 2017",
-    content: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur sapien eget ipsum porttitor ultricies. Morbi aliquet vestibulum interdum. Nam id augue in magna consectetur tristique. Pellentesque tempor pharetra libero a ultrices. Donec a semper lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum mattis quam at quam pulvinar, ac auctor lacus convallis. Morbi pharetra mi vitae turpis fermentum, sed ullamcorper nisl hendrerit. Nullam egestas diam justo, vitae ultricies lectus tincidunt quis. In consequat in justo non posuere. Morbi turpis leo, mollis quis mattis sit amet, accumsan eleifend quam. Quisque non magna augue. Integer maximus luctus libero quis faucibus. Suspendisse posuere quis ligula nec ullamcorper. Mauris ut dolor eu orci accumsan hendrerit ut quis justo..</p>"
+    content: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur sapien eget ipsum porttitor ultricies. Morbi aliquet vestibulum interdum. Nam id augue in magna consectetur tristique. Pellentesque tempor pharetra libero a ultrices. Donec a semper lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum mattis quam at quam pulvinar, ac auctor lacus convallis. Morbi pharetra mi vitae turpis fermentum, sed ullamcorper nisl hendrerit. Nullam egestas diam justo, vitae ultricies lectus tincidunt quis. In consequat in justo non posuere. Morbi turpis leo, mollis quis mattis sit amet, accumsan eleifend quam. Quisque non magna augue. Integer maximus luctus libero quis faucibus. Suspendisse posuere quis ligula nec ullamcorper. Mauris ut dolor eu orci accumsan hendrerit ut quis justo..</p>`
 };
+
+var htmlTemplate = `<!DOCTYPE html>
+<html>
+  <head>
+      <title>${title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="/ui/style.css" rel="stylesheet">
+  </head>
+  <body>
+      <div class="container">
+          <div>
+            <a href="/ui/index.html">Home</a>
+          </div>
+          <hr>
+          <div>
+              <h4>${heading}</h4>
+          </div>
+          <div>
+            ${date}  
+          </div>
+          <div>
+            ${content}
+          </div>
+      </div>
+  </body>
+</html>
+`;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', htmlTemplate));
 });
 
 app.get('/article-two', function (req, res) {
