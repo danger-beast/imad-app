c293eae6858d3efebca85561ceeffd1b25004348
Muhamad Uzair <cool.uzair2000@gmail.com>
2017-08-17 13:01:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-    res.sendFile('This is article one.');
+app.get('/article-one', function (req, res) {
+  res.send("This is first article");
 });
 
 app.get('/ui/style.css', function (req, res) {
